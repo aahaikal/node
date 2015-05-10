44b75eb014e72b18c4c07182a4de5ff04d27c4f3
Alan Haikal <alan.haikal@kipusystems.com>
2015-05-10 13:23:35 -0400

    find and bookroutes

--- app.js
+++ app.js
@@ -1,66 +1,21 @@
 var express = require('express'),
-    mongoose = require('mongoose');
+    mongoose = require('mongoose'),
+    bodyParser = require('body-parser');
 
 var db = mongoose.connect('mongodb://localhost/bookAPI');
 
 var Book = require('./models/bookModel');
 var app = express();
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 var port = process.env.PORT || 9000;
 
-var bookRouter = express.Router();
-
-bookRouter.route('/Books')
-    .get(function(req, res) {
-
-        var query = {};
+bookRouter = require("./routes/bookRouters")(Book);
 
 
-        if (req.query.genre) {
-            console.log(req);
-            query.genre = req.query.genre;
-        }
-
-        Book.find(query, function(err, books) {
-            if (err) {
-                res.status(500).send(err);
-            } else {
-                res.json(books);
-            }
-
-        });
-    });
-
-bookRouter.route('/Books/:bookId')
-    .get(function(req, res) {
-
-        Book.findById(req.params.bookId, function(err, book) {
-            if (err) {
-            	console.log(err);
-                res.status(500).send(err);
-            } else {
-                res.json(book);
-            }
-
-        });
-    });
-
-bookRouter.route('/Apps')
-    .get(function(req, res) {
-        var response = {
-            app1: {
-                name: 'just an app',
-                version: '1.2.3'
-            },
-            app2: {
-                name: 'just an app 2',
-                version: '0.0.1'
-            }
-        };
-        res.json(response);
-    });
-
-app.use('/api', bookRouter);
+app.use('/api/books', bookRouter);
 
 app.get('/', function(req, res) {
     res.send('<h1>welcome to my API</h1><h2> something</h2>');
